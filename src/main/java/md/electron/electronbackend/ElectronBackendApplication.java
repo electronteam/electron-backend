edf7a735c8eddbd361822dda6a6b619968f9ae71
Radu Baroncea <rbaroncea@tacitknowledge.com>
2019-03-27 13:54:19 +0200

    Created electron-backend as a spring boot app

---
+++ src/main/java/md/electron/electronbackend/ElectronBackendApplication.java
@@ -0,0 +1,13 @@
+package md.electron.electronbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/md/electron/electronbackend/ElectronBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package md.electron.electronbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElectronBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
