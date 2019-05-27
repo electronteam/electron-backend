67a38ff70e001650196c83f7a16876e8130e290d
Radu Baroncea <rbaroncea@tacitknowledge.com>
2019-05-27 14:19:37 +0300

    Added some more products for testing

--- src/main/resources/dbscripts/import.sql
+++ src/main/resources/dbscripts/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO product (id, code, description, name) VALUES ('1', '1', 'Produs de testare', 'primul produs');
 INSERT INTO product (id, code, description, name) VALUES ('2', '2', 'Produs de testare 2', 'al doilea produs');
 INSERT INTO product (id, code, description, name) VALUES ('3', '3', 'Produs de testare 3', 'al treilea produs');
+INSERT INTO product (id, code, description, name) VALUES ('4', '4', 'Produs de testare 4', 'al patrulea produs');
+INSERT INTO product (id, code, description, name) VALUES ('5', '5', 'Produs de testare 5', 'al cincilea produs');
+INSERT INTO product (id, code, description, name) VALUES ('6', '6', 'Produs de testare 6', 'al saselea produs');
