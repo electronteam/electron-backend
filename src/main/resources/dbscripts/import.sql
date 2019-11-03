628a95b138d6d7889f86e054477250050a42ecbb
Radu Baroncea <rbaroncea@tacitknowledge.com>
2019-11-03 11:11:35 +0200

    Changed one insert script with more friendly data

--- src/main/resources/dbscripts/import.sql
+++ src/main/resources/dbscripts/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO product (id, code, description, name) VALUES ('1', '1', 'Produs de testare', 'primul produs');
+INSERT INTO product (id, code, description, name) VALUES ('1', '1', 'Telefon Dual SIM cu sistem de operare Android, ecran tactil 6.28 inch, memorie internă 128 GB, cameră dublă cu rezoluţia 16 și 20 megapixeli, GPS, senzor de amprentă digitală, susține tehnologia 4G', 'Samsung Galaxy');
 INSERT INTO product (id, code, description, name) VALUES ('2', '2', 'Produs de testare 2', 'al doilea produs');
 INSERT INTO product (id, code, description, name) VALUES ('3', '3', 'Produs de testare 3', 'al treilea produs');
 INSERT INTO product (id, code, description, name) VALUES ('4', '4', 'Produs de testare 4', 'al patrulea produs');
