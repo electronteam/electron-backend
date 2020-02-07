9f6585d64ccf5f67d0ac559dec309f140c073499
Radu Baroncea <rbaroncea@tacitknowledge.com>
2020-02-07 22:41:49 +0200

    Changed all products API Url

--- src/main/java/md/electron/electronbackend/constants/RequestMappings.java
+++ src/main/java/md/electron/electronbackend/constants/RequestMappings.java
@@ -2,7 +2,6 @@ package md.electron.electronbackend.constants;
 
 public class RequestMappings
 {
-    public static final String PRODUCTS = "api/products";
     public static final String PRODUCT_DETAILS = "api/product/{code}";
     public static final String SOLR_PRODUCTS = "api/solr/products";
     public static final String SOLR_SEARCH_PRODUCTS = "api/solr/searchProducts";
@@ -10,6 +9,7 @@ public class RequestMappings
     public static final String CURRENT_CART = "/api/currentCart";
     public static final String PLACE_ORDER = "/api/placeOrder";
 
+    public static final String PRODUCTS = "/api/admin/products";
     public static final String ADMIN_ORDERS = "/api/admin/orders";
     public static final String ADMIN_CREATE_USER = "/api/admin/createUser";
 
