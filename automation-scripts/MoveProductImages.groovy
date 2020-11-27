a105ac0f4a7a0ecd74f8406aceaadcca36f38b28
Radu Baroncea <rbaroncea@tacitknowledge.com>
2020-11-27 22:11:37 +0200

    Now, automation script creates local medias directory if it doesn't exist

--- automation-scripts/MoveProductImages.groovy
+++ automation-scripts/MoveProductImages.groovy
@@ -4,12 +4,19 @@ import java.nio.file.Paths
 println "Moving product images !!!"
 
 def srcDirectory = new File('src/main/resources/static/medias')
-def destDirectory = "data/medias/products/"
+def destDirectoryName = "data/medias/products/"
+def destDirectory = new File(destDirectoryName)
+
+if (!destDirectory.exists())
+{
+  println "Creating local medias directory !!!"
+  Files.createDirectories(Paths.get(destDirectoryName))
+}
 
 srcDirectory.eachFile {
   filename = it.name
   println(filename)
-  destFileName = destDirectory + filename
+  destFileName = destDirectoryName + filename
   destFile = new File(destFileName)
 
   if (!destFile.exists())
